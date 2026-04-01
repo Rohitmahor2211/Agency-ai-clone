73ac558047c39e079263e7bf0ed04825efa25f8e
RohitM <rohitmahor022@gmail.com>
2026-04-01 11:19:57 +0530

    change import image name

--- src/components/Company_card.jsx
+++ src/components/Company_card.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import workMobileImg from "../assets/worK_mobile_app.png";
+import workMobileAPP from "../assets/worK_mobile_app.png";
 import work_fitness_app from "../assets/work_fitness_app.png";
 import work_dashboard_management from "../assets/work_dashboard_management.png";
 import {ThemeContextProvider} from '../comtextApi/ThemeContext'
@@ -7,7 +7,7 @@ const Company_card = () => {
     const {theme} = useContext(ThemeContextProvider)
   const cardInfo = [
     {
-      imageUrl:  workMobileImg ,
+      imageUrl:  workMobileAPP ,
       mainHead: "Mobile app marketing",
       companyCardInfo:
         "We turn bold ideas into powerful digital solutions that connect, engage..",
